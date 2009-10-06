c89021eb7c8fc483387800c4819eab587dff1736
Carl Woodward <carl@carl.local>
2009-10-07 07:56:21 +1100

    Toolmantim for the win

--- test.rb
+++ test.rb
@@ -12,6 +12,8 @@ ActiveRecord::Base.default_timezone = :utc
 ActiveRecord::Base.logger = Logger.new STDOUT
 
 ActiveRecord::Base.establish_connection :adapter => "sqlite3", :database  => "test.sqlite3"
+# ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database => "proper_time_zones_test", :username => "carl",
+#                                         :password => '')
 
 class CreateEvents < ActiveRecord::Migration
   def self.up
@@ -40,10 +42,20 @@ describe "time zones messing up" do
   after do
     Event.destroy_all
   end
+  describe "find by date field" do
+    it "should find an event on today" do
+      today = Time.parse Date.today.strftime('%Y-%m-%d')
+      Event.find_by_scheduled_on(today.to_date).should.equal @event
+    end
+  end
   describe "find by the time field" do
     it "should find an event with time on a date with a parsed time" do
-      today = DateTime.parse Date.today.strftime('%Y-%m-%d')
-      Event.find(:first, :conditions => ["date(scheduled_at) = date(?)", today.utc.to_date]).should.equal @event
+      today = Time.parse Date.today.strftime('%Y-%m-%d')
+      Event.find(:first, :conditions => ["date(scheduled_at) = date(?)", today.utc]).should.equal @event
+    end
+    it "should find an event with time on a date with today" do
+      today = Time.today
+      Event.find(:first, :conditions => ["date(scheduled_at) = date(?)", today.utc]).should.equal @event
     end
   end
 end
